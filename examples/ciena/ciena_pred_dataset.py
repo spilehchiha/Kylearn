a65d6b8231424e78c8671fb61e7e127f9ed178cd
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-06-11 21:42:26 -0400

    examples

--- examples/ciena/ciena_pred_dataset.py
+++ examples/ciena/ciena_pred_dataset.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from framework.dataset import Dataset
-from utils.resampling import random_upsampling
+from utils.resampling import random_upsampling, random_downsampling
 import collections
 
 class pred_Dataset(Dataset):
@@ -10,16 +10,63 @@ class pred_Dataset(Dataset):
         super().__init__()
 
         X = np.load(x_path)
-        y = np.load(y_path).flatten()
-        print(collections.Counter(y))
-        self.shape = X.shape
-        X, y = random_upsampling(X, y)
-        X = X.reshape([-1]+list(self.shape[1:]))
-        print(collections.Counter(y))
+        y = np.load(y_path)
+        shape = X.shape
+        index_num = np.arange(shape[0])
+        index = y == 1
+        index_pos = np.extract(index, index_num)
+        index_neg = np.extract(~index, index_num)
 
+        idx_pos_train, idx_pos_test = train_test_split(index_pos, test_size= 0.3, random_state= 6)
+        idx_pos_train, idx_pos_val = train_test_split(idx_pos_train, test_size= 0.2, random_state= 6)
+        idx_neg_train, idx_neg_test = train_test_split(index_neg, test_size= 2000, random_state= 6)
+        idx_train = idx_pos_train.tolist()+idx_neg_train.tolist()
+        idx_test = idx_pos_test.tolist()+idx_neg_test.tolist()
+
+        X_train, y_train = random_upsampling(X[idx_train], y[idx_train])
+        X_val, y_val = X[idx_pos_val], y[idx_pos_val]
+        X_test, y_test = X[idx_test], y[idx_test]
+
+
+        self.train_set = np.zeros(X_train.shape[0], dtype=[
+            ('x', np.float32, (X_train.shape[1:])),
+            ('y', np.int32, ([1]))
+        ])
+        self.train_set['x'] = X_train
+        self.train_set['y'] = y_train
+
+        self.val_set = np.zeros(X_val.shape[0], dtype=[
+            ('x', np.float32, (X_val.shape[1:])),
+            ('y', np.int32, ([1]))
+        ])
+        self.val_set['x'] = X_val
+        self.val_set['y'] = y_val.reshape([-1, 1])
+
+        self.test_set = np.zeros(X_test.shape[0], dtype=[
+            ('x', np.float32, (X_test.shape[1:])),
+            ('y', np.int32, ([1]))
+        ])
+
+        self.test_set['x'] = X_test
+        self.test_set['y'] = y_test.reshape([-1, 1])
+
+        # pos_sample = self.train_set[self.train_set['y'] == 1]
+        # neg_sample = self.train_set[self.train_set['y'] == 0]
+        # pos_train, pos_test = train_test_split(pos_sample, test_size=0.4, random_state=42)
+        # self.train_set, self.test_set = train_test_split(self.train_set, test_size=20000, random_state=41)
+        # self.train_set, self.eval_set = train_test_split(self.train_set, test_size=0.1, random_state=40)
+
+
+class pred_Dataset_2(Dataset):
+    def __init__(self, x_path, y_path):
+        super().__init__()
+
+        X = np.load(x_path)
+        y = np.load(y_path)
+        X, y = random_upsampling(X, y)
         self.train_set = np.zeros(X.shape[0], dtype=[
             ('x', np.float32, (X.shape[1:])),
-            ('y', np.int32, (1))
+            ('y', np.int32, ([1]))
         ])
         self.train_set['x'] = X
         self.train_set['y'] = y
@@ -27,8 +74,6 @@ class pred_Dataset(Dataset):
 
         # pos_sample = self.train_set[self.train_set['y'] == 1]
         # neg_sample = self.train_set[self.train_set['y'] == 0]
-
-        # _, neg_sample = train_test_split(neg_sample, test_size=pos_sample.shape[0], random_state=42)
-        _, self.train_set= train_test_split(self.train_set, test_size=20000, random_state= 42)
-        self.train_set, self.test_set = train_test_split(self.train_set, test_size=0.2, random_state=41)
-        self.train_set, self.eval_set = train_test_split(self.train_set, test_size=0.1, random_state=40)
+        # pos_train, pos_test = train_test_split(pos_sample, test_size=0.4, random_state=42)
+        self.train_set, self.test_set = train_test_split(self.train_set, test_size=5000, random_state=41)
+        self.train_set, self.val_set = train_test_split(self.train_set,train_size=100000, test_size=5000, random_state=40)
--- examples/ciena/predict.py
+++ examples/ciena/predict.py
@@ -1,20 +1,22 @@
 from Resnet.resnet_model import ResnetModel
 from Resnet.resnet_network import Resnet
+from CNN.cnn_model import CNNModel
+from CNN.cnn_network import Cnn_3layers
 from ciena_pred_dataset import pred_Dataset
 from utils.utils import get_variable_num
 device_type = 'OPTMON'
 dataset = pred_Dataset(
-                       x_path= '/home/oem/Projects/NetDeviceAbnormalDetection/data/perdevice/%s_pms_3_45.npy'%device_type,
-                       y_path= '/home/oem/Projects/NetDeviceAbnormalDetection/data/perdevice/%s_alarms_2days.npy'%device_type)
-network = Resnet()
-
-model = ResnetModel(ckpt_path='/home/oem/Projects/Kylearn/examples/ciena/models/%s_pred'%device_type,tsboard_path='logs',
-                    network=network, num_classes=1, input_shape=list(dataset.shape[1:]), lr=0.002, batch_size=500)
-
-model.save_tensorboard_graph()
-
-model.initialize_variables()
-model.train(dataset,0.002)
-
-# model.restore_checkpoint(9090)
-model.plot(dataset, threshold=0.6)
+                       x_path= '/home/oem/Projects/NetDeviceAbnormalDetection/data/perdevice/%s_pms_3_partial_v2.npy'%device_type,
+                       y_path= '/home/oem/Projects/NetDeviceAbnormalDetection/data/perdevice/%s_alarms_2days_v2.npy'%device_type)
+# network = Cnn_3layers()
+#
+# model = CNNModel(ckpt_path='/home/oem/Projects/Kylearn/examples/ciena/models/%s_pred'%device_type,tsboard_path='logs',
+#                     network=network, num_classes=1, input_shape=list(dataset.shape[1:]), lr=0.002, batch_size=500)
+#
+# model.save_tensorboard_graph()
+#
+# model.initialize_variables()
+# model.train(dataset,0.002)
+#
+# # model.restore_checkpoint(9090)
+# model.plot(dataset, threshold=0.6)
