935df5eae651f79d89b86867a2fc1f70f5d38377
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-06-19 16:29:35 -0400

    add param

--- utils/resampling.py
+++ utils/resampling.py
@@ -1,19 +1,19 @@
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
 
-def random_upsampling(X, y):
+def random_upsampling(X, y, random_state):
     shape = X.shape
     X = X.reshape([shape[0],-1])
-    ros = RandomOverSampler(random_state=42)
+    ros = RandomOverSampler(random_state = random_state)
     X_res, y_res = ros.fit_resample(X, y)
     X_res = X_res.reshape([X_res.shape[0]]+list(shape[1:]))
     y_res = y_res.reshape([-1,1])
     return X_res, y_res
 
-def random_downsampling(X, y):
+def random_downsampling(X, y, random_state):
     shape = X.shape
     X = X.reshape([shape[0], -1])
-    rus = RandomUnderSampler(random_state=42)
+    rus = RandomUnderSampler(random_state = random_state)
     X_res, y_res = rus.fit_resample(X, y)
     X_res = X_res.reshape([X_res.shape[0]] + list(shape[1:]))
     y_res = y_res.reshape([-1, 1])
