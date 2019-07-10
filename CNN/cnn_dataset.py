4ebb931ff696168f6b88b3e91a582e8d9277f302
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-07-10 18:51:24 -0400

    update

--- CNN/cnn_dataset.py
+++ CNN/cnn_dataset.py
@@ -15,7 +15,6 @@ class CNN_dataset(Dataset):
         y = np.load(label_path + '_train.npy').reshape([-1,out_num])
         assert X.shape[0] == y.shape[0]
         # X, dev, y = multi_inputs_random_upsampling(X, dev, y, 12)
-        print(collections.Counter(y.flatten()))
         self.train_set = np.zeros(X.shape[0], dtype=[
             ('x', np.float32, (X.shape[1:])),
             ('y', np.int32, ([out_num]))
@@ -26,7 +25,6 @@ class CNN_dataset(Dataset):
         # test_set
         X2 = np.load(feature_path + '_test.npy')
         y2 = np.load(label_path + '_test.npy').reshape([-1,out_num])
-        print(collections.Counter(y2.flatten()))
         assert X2.shape[0] == y2.shape[0]
 
         self.test_set = np.zeros(X2.shape[0], dtype=[
