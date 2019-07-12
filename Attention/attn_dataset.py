c9aacf4c854c7187987233c54f8ee0b1af5b964a
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-07-12 17:12:00 -0400

    update

--- Attention/attn_dataset.py
+++ Attention/attn_dataset.py
@@ -48,22 +48,22 @@ class Attn_dataset_2d(Dataset):
         super().__init__()
 
         # train set
-        X = np.load(feature_path)
+        X = np.expand_dims(np.load(feature_path), axis=-1)
         dev = np.load(dev_path)
-        y = np.load(label_path).flatten()
+        y = np.load(label_path)
         assert X.shape[0] == y.shape[0]
         assert dev.shape[0] == y.shape[0]
         dataset = np.zeros(X.shape[0], dtype=[
             ('x', np.float32, (X.shape[1:])),
             ('dev', np.int32, (dev.shape[1])),
-            ('y', np.int32, ())
+            ('y', np.int32, ([1]))
         ])
         dataset['x'] = X
         dataset['dev'] = dev
         dataset['y'] = y
 
-        anomaly = dataset[dataset['y'] != 0]
-        normal = dataset[dataset['y'] == 0]
+        anomaly = dataset[dataset['y'].flatten() != 0]
+        normal = dataset[dataset['y'].flatten() == 0]
         del dataset
         normal,_ = train_test_split(normal, train_size= anomaly.shape[0], random_state=22)
         self.train_set,self.test_set = train_test_split(
