9e82454d3d38500e529139285011532100264927
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-07-23 09:36:06 -0400

    update

--- visualization/draw_roc.py
+++ visualization/draw_roc.py
@@ -1,6 +1,9 @@
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as pyplot
+pyplot.rcParams['savefig.dpi'] = 300  # pixel
+pyplot.rcParams['figure.dpi'] = 300  # resolution
+pyplot.rcParams["figure.figsize"] = [1,1] # figure size
 
-def plot_roc_curve(fprs, tprs, auc, x_axis = 1):
+def plot_roc_curve(fprs, tprs, auc, x_axis = 1, plt = pyplot):
 
     plt.plot(fprs, tprs, color="darkorange", label='ROC curve (area = %0.3f)' % auc)
     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
